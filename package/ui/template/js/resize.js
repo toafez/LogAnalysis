51dde4ed082cba74ce6f1a104f5f21dbefc47ba5
Horst <info@schmidhorst.de>
2023-01-24 21:26:21 +0100

    Resize also if the search is used
    Resizing works now also if the search is used and "px-1 py-1" is used instead of "px-0 py-1"

--- package/ui/template/js/resize.js
+++ package/ui/template/js/resize.js
@@ -2,15 +2,24 @@
 
 function setBoxHeight() {
   var h0=window.innerHeight;
+  var h=document.body.scrollHeight;
   var o1=document.getElementById("file-content-box");
   if (!!o1) {
     var h1=o1.style.height;
     var t1=o1.getBoundingClientRect().top;
-    let h2=h0-t1-25;
-    o1.style.height = h2+"px";
-    var o2=document.getElementById("file-content-txt");
-    if (!!o2) {
-      o2.style.height=h2+"px";
+    let h2=h0-t1-22;
+    if (h2>50) {
+      o1.style.height = h2+"px";
+      var o2=document.getElementById("file-content-txt");
+      if (!!o2) {
+        o2.style.height=h2+"px";
+        if (h>h0+20) { // the list of available files causes scrollbar for whole dialog
+          o2.style.resize="vertical"
+        }
+        else {
+          o2.style.resize="none"
+        }
+      }
     }
   }
 }
