67cf566fa8f3d97bc02de07067d38bdabdab57a5
toafez <toafez@gmx.de>
2023-06-06 18:50:21 +0200

    Source code optimization

--- ui/LogAnalysis.js
+++ ui/LogAnalysis.js
@@ -1,49 +1,39 @@
 Ext.namespace("SYNO.SDS.LogAnalysis.Utils");
 
-Ext.apply(SYNO.SDS.LogAnalysis.Utils, function(){
-    return{
-        getMainHtml: function(){
-            // Timestamp must be inserted here to prevent caching of iFrame
-            return '<iframe src="webman/3rdparty/LogAnalysis/index.cgi?_ts=' + new Date().getTime() + '" title="react-app" style="width: 100%; height: 100%; border: none; margin: 0"/>';
-        },
-    }
-}());
-
 Ext.define("SYNO.SDS.LogAnalysis.Application", {
     extend: "SYNO.SDS.AppInstance",
     appWindowName: "SYNO.SDS.LogAnalysis.MainWindow",
-    constructor: function(){
+    constructor: function() {
         this.callParent(arguments);
     }
 });
- 
+
 Ext.define("SYNO.SDS.LogAnalysis.MainWindow", {
     extend: "SYNO.SDS.AppWindow",
-    constructor : function(a){
-        var MY = SYNO.SDS.LogAnalysis;
+    constructor: function(a) {
         this.appInstance = a.appInstance;
-        MY.MainWindow.superclass.constructor.call(this, Ext.apply({
-            layout : "fit",
-            resizable : true,
-            cls: "syno-my-win",
-            maximizable : true,
-            minimizable : true,
-            width : 1024,
-            height : 768,
-            html: MY.Utils.getMainHtml()
+        SYNO.SDS.LogAnalysis.MainWindow.superclass.constructor.call(this, Ext.apply({
+            layout: "fit",
+            resizable: true,
+            cls: "syno-app-win",
+            maximizable: true,
+            minimizable: true,
+            width: 1024,
+            height: 768,
+            html: SYNO.SDS.LogAnalysis.Utils.getMainHtml()
         }, a));
-        MY.Utils.ApplicationWindow = this;
+        SYNO.SDS.LogAnalysis.Utils.ApplicationWindow = this;
     },
 
-    onOpen : function(){
+    onOpen: function() {
         SYNO.SDS.LogAnalysis.MainWindow.superclass.onOpen.apply(this, arguments);
     },
 
-    onRequest : function(a){
+    onRequest: function(a) {
         SYNO.SDS.LogAnalysis.MainWindow.superclass.onRequest.call(this, a);
     },
 
-    onClose : function(){
+    onClose: function() {
         clearTimeout(SYNO.SDS.LogAnalysis.TimeOutID);
         SYNO.SDS.LogAnalysis.TimeOutID = undefined;
         SYNO.SDS.LogAnalysis.MainWindow.superclass.onClose.apply(this, arguments);
@@ -51,3 +41,12 @@ Ext.define("SYNO.SDS.LogAnalysis.MainWindow", {
         return true;
     }
 });
+
+Ext.apply(SYNO.SDS.LogAnalysis.Utils, function() {
+    return {
+        getMainHtml: function() {
+            // Timestamp must be inserted here to prevent caching of iFrame
+            return '<iframe src="webman/3rdparty/LogAnalysis/index.cgi?_ts=' + new Date().getTime() + '" title="react-app" style="width: 100%; height: 100%; border: none; margin: 0"/>';
+        },
+    }
+}());
